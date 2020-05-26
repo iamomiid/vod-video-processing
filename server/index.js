dfbe10d59a4ed1c1d6eb3fe6361a470c0a687094
Omid Sayfun <seyfanomid@ymail.com>
2020-05-26 14:10:39 +0430

    Generalized Server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const dashConfig = {
+	syb4lbaiTMWk3HCZVygFnA: 'YX2KElooTfSOPGsYZjSKPw',
+};
+
+const app = express();
+
+app.use((req, res, next) => {
+	// CORS
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	return next();
+});
+
+app.use(bodyParser.raw({ type: 'application/octet-stream' }));
+
+app.post('/dash', (req, res) => {
+	const { type, kids } = JSON.parse(req.body.toString());
+	const output = { type, keys: [] };
+	for (const key of kids) {
+		if (Object.keys(dashConfig).includes(key)) {
+			output.keys.push({ kid: key, k: dashConfig[key], kty: 'oct' });
+		}
+	}
+	return res.send(output);
+});
+
+app.post('/hls', (req, res) => {
+	return res.send('hi');
+});
+
+const server = http.createServer(app);
+
+server.listen(1111, () => {
+	console.log('Listening on 1111');
+});
